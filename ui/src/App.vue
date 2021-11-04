572dab94d589402bb59e0f247345457b03a2f9db
Troplo <troplo@troplo.com>
2021-11-04 13:58:00 +1100

    Vue

---
+++ ui/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+		<div class="nav">
+			<h1>Nuem</h1>
+			<div class="menu">
+				<router-link to="/create_game">Host Game</router-link>
+				<router-link to="/join_game">Join Game</router-link>
+				<router-link to="/scenarios">Scenarios</router-link>
+				<router-link to="https://nuem.pinnoto.org/community">Community</router-link>
+				<router-link to="/settings" v-if="$store.state.user">username</router-link>
+        <router-link to="/login" v-if="!$store.state.user">Login</router-link>
+        <router-link to="/register" v-if="!$store.state.user">Register</router-link>
+			</div>
+		</div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+@import './assets/style/style.css'
+</style>
---
+++ ui/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'About',
+  mounted() {
+    document.title = "About"
+  }
+}
+</script>
